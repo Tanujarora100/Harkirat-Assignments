340e8e5f5c0eaaab588cd3e876a8435eb9386d74
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-25 02:53:03 +0530

    week 6 Assignments

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Input from "./Input.tsx";
+
+function App() {
+
+
+    return (
+        <div>
+        <Input></Input>
+        </div>
+    );
+}
+
+export default App;
