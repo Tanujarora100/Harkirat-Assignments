7842d82e7c65d9e312a1b843618108c02bc872f8
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-25 11:28:25 +0530

    Use Memo Assignments done

--- src/useMemo/Assignment3.tsx
+++ src/useMemo/Assignment3.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, {useState, useMemo} from 'react';
 
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
@@ -10,12 +10,17 @@ interface Item {
 
 export const Assignment3: React.FC = () => {
     const [items, setItems] = useState<Item[]>([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
+        {name: 'Chocolates', value: 10},
+        {name: 'Chips', value: 20},
+        {name: 'Onion', value: 30},
+        {name: 'Tomato', value: 20},
+        {name:'TV',value:570}
         // Add more items as needed
     ]);
+    // Yes, you're correct. When the dependencies of useMemo change, it triggers a recalculation of the memoized value, and if that value is used in the render,
+    // it will cause the component to re-render with the updated value.
+    // The purpose of useMemo is not to prevent re-renders but to optimize expensive computations by memoizing the result
+    const totalValue = useMemo(() => items.reduce((acc, item) => acc + item.value, 0), [items]);
     return (
         <div>
             <ul>
